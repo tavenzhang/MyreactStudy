41653c3692d88d8ed11afc653aa6994902f7b6fb
thomas <258333601@163.com>
2017-07-04 09:29:11 +0700

    33

--- js/assets/index.js
+++ js/assets/index.js
@@ -1,26 +1,14 @@
 /**
  * Created by zhangxinhua on 16/12/12.
  */
-export const startImg = require('./welcome/start.png')
-
-export const home_game = require('./homeGame.png')
-let albumn = require('./tabBar/album.png')
-let albumn_gray = require('./tabBar/album_gray.png')
-
-let home = require('./tabBar/home.png')
-let home_gray = require('./tabBar/home_gray.png')
-
-let me = require('./tabBar/me.png')
-let me_gary = require('./tabBar/me_gray.png')
-
 
 export let ImgTabbar = {
-    albumn,
-    albumn_gray,
-    home,
-    home_gray,
-    me,
-    me_gary,
+    albumn:require('./tabBar/album@2x.png'),
+    albumn_gray:require('./tabBar/album_gray@2x.png'),
+    home:require('./tabBar/home@2x.png'),
+    home_gray:require('./tabBar/home_gray@2x.png'),
+    me:require('./tabBar/me@2x.png'),
+    me_gary:require('./tabBar/me_gray@2x.png'),
 }
 
 export let ImgHome = {
@@ -47,7 +35,7 @@ export let ImgAbuotMe = {
     vip_gray: require('./me/vip_gray.png'),
     config: require('./me/config.png'),
     login_welcom: require('./me/login/welcom_bg.png'),
-    welcome: require('./me/login/welcom.png'),
+    welcome: require('./me/login/welcom@2x.png'),
 }
 
 export let ImgAblum = {
@@ -69,5 +57,5 @@ export let SoundMusic = {
     playViewbtn: require('./music/playViewbtn.png'),
     sharebtn:require('./music/player/share.png'),
     play:require('./music/player/player.png'),
-    pause:require('./music/player/pause.png'),
+    pause:require('./music/player/pause@2x.png'),
 }

9bf780cbf024be77cca8db0279c4b6e9b8f1d931
小灰 <xiaohui@xiaohuideMacBook-Pro.local>
2017-07-08 12:01:42 +0700

    链接开户样式调整

--- js/view/page/me/operatView/agentCreatUser/LinkDetailView.js
+++ js/view/page/me/operatView/agentCreatUser/LinkDetailView.js
@@ -28,28 +28,28 @@ export default class LinkDetailView extends BaseView {
         }
         return (<View style={G_Style.appContentView}>
 
-            <View style={{height:80 ,borderBottomColor:G_Theme.gray,paddingTop:20}}>
+            <View style={{height:100 ,borderBottomColor:G_Theme.gray,paddingTop:20,paddingBottom:20,}}>
                 <Text style={[{textAlign:'center',fontSize:30,color:G_Theme.primary}]}> {content.created_count}</Text>
-                <Text style={[{textAlign:'center'}]}>开户数</Text>
+                <Text style={[{textAlign:'center',fontSize:12,color:G_Theme.grayDeep}]}>开户数</Text>
             </View>
 
-            <View style={{height:30 ,borderBottomColor:G_Theme.gray,padding:5,flexDirection:'row'}}>
-                <Text style={[{flex:1,alignSelf:'center'}]}>  是否代理</Text>
-                <Text style={{flex:1,alignSelf:'center'}} >状态</Text>
-                <Text style={{flex:1,alignSelf:'center'}}> 有效期</Text>
+            <View style={{height:25 ,borderBottomColor:G_Theme.gray,flexDirection:'row'}}>
+                <Text style={[{flex:1,textAlign:'center'}]} numberOfLines={1}>{content.is_agent ? "代理":"玩家"}</Text>
+                <Text style={{flex:1,textAlign:'center',fontSize:16,color:content.status == 2 || content.status == 1 ? G_Theme.grayDeep : G_Theme.primary}} numberOfLines={1}>{aStatus[content.status]}</Text>
+                <Text style={{flex:1,textAlign:'center'}} numberOfLines={1}>{validstr}</Text>
+
+            </View>
+            <View style={{height:25 ,borderBottomColor:G_Theme.gray,borderBottomWidth:1,flexDirection:'row'}}>
+
+                <Text style={[{flex:1,textAlign:'center',fontSize:12,color:G_Theme.grayDeep}]}>是否代理</Text>
+                <Text style={{flex:1,textAlign:'center',fontSize:12,color:G_Theme.grayDeep}} >状态</Text>
+                <Text style={{flex:1,textAlign:'center',fontSize:12,color:G_Theme.grayDeep}}> 有效期</Text>
 
 
             </View>
-            <View style={{height:30 ,borderBottomColor:G_Theme.gray,padding:5,borderBottomWidth:1,flexDirection:'row'}}>
-                <Text style={[{flex:1,alignSelf:'center'}]} numberOfLines={1}>  {content.is_agent ? "代理":"玩家"}</Text>
-                <Text style={{flex:1,alignSelf:'center'}} numberOfLines={1}>   {aStatus[content.status]}</Text>
-                <Text style={{flex:1,alignSelf:'center'}} numberOfLines={1}>   {validstr}</Text>
 
-
-            </View>
-
-            <View style={{height:60 ,borderBottomColor:G_Theme.gray,padding:10,borderBottomWidth:1,flexDirection:'row'}}>
-                <Text numberOfLines={2}>   {content.url}</Text>
+            <View style={{height:70 ,borderColor:G_Theme.primary,backgroundColor:G_Theme.primary,padding:10,borderWidth:1,flexDirection:'row'}}>
+                <Text style={{color:'#fff',}}numberOfLines={2}>{content.url}</Text>
 
             </View>
             <View style={{height:30 ,borderBottomColor:G_Theme.gray,padding:5,borderBottomWidth:1,flexDirection:'row'}}>
@@ -68,10 +68,7 @@ export default class LinkDetailView extends BaseView {
                 <Text style={styles.titleStyle}>创建时期:</Text>
                 <Text style={styles.descStyle}>  {content.created_at}</Text>
             </View>
-            <View style={{height:30 ,borderBottomColor:G_Theme.gray,padding:5,borderBottomWidth:1,flexDirection:'row'}}>
-                <Text style={styles.titleStyle}> 最后更新日期:</Text>
-                <Text style={styles.descStyle}>  {content.agent_qqs}</Text>
-            </View>
+
 
         </View>)
     }
--- js/view/page/me/operatView/agentCreatUser/LinkListView.js
+++ js/view/page/me/operatView/agentCreatUser/LinkListView.js
@@ -45,33 +45,38 @@ export default class LinkListView extends BaseView {
 
     rendeRow = (data) => {
 
-        const price_group = JSON.parse(data.prize_group_sets);
-        const nowDate = Date.parse(new Date()),
-            expirtDate = G_DateUtil.datetime2unix(data.expired_at);
-        if (nowDate > expirtDate) {
-            data.status = 2; //过期
-        }
+        // const price_group = JSON.parse(data.prize_group_sets);
+        // const nowDate = Date.parse(new Date()),
+        //     expirtDate = G_DateUtil.datetime2unix(data.expired_at);
+        // if (nowDate > expirtDate) {
+        //     data.status = 2; //过期
+        // }
         // TLog('this.state.aStatus',this.state.aStatus);
         return (<View style={[styles.row, {backgroundColor: data.status == 2 || data.status == 1 ? '#ccc' : '#fff'}]}>
-            <TouchableHighlight  onPress={() => {
+            <TouchableHighlight onPress={() => {
                 this._onDeleteLink(data)
             }}>
-            <View style={[styles.contentView, {flex: 1,}]}>
-                <AIcon name={'minus-circle'}
-                       style={{fontSize: 20, color: G_Theme.grayDeep,}}/>
-            </View>
+                <View style={[styles.contentView, {flex: 1,}]}>
+                    <AIcon name={'minus-circle'}
+                           style={{fontSize: 20, color: G_Theme.grayDeep,}}/>
+                </View>
             </TouchableHighlight>
             <View style={[styles.contentView, {flex: 2}]}>
                 <View style={{flexDirection: "row",}}>
                     <Text style={[styles.agentText]}>
                         {data.is_agent > 0 ? "代理" : "玩家"}
                     </Text>
-                    <Text style={{fontSize: 12, color: G_Theme.grayDeep, lineHeight: 20,marginLeft:3}}>{this.state.aStatus[data.status]}</Text>
+                    <Text style={{
+                        fontSize: 12,
+                        color: G_Theme.grayDeep,
+                        lineHeight: 20,
+                        marginLeft: 3
+                    }}>{this.state.aStatus[data.status]}</Text>
 
                 </View>
 
                 <Text numberOfLines={1} style={{fontSize: 12, color: G_Theme.grayDeep, lineHeight: 20}}>
-                    奖金组:{price_group[0].prize_group}
+                    奖金组:{data.price_group[0].prize_group}
                 </Text>
             </View>
             <View style={[styles.contentView, {flex: 5,}]}>
@@ -81,11 +86,11 @@ export default class LinkListView extends BaseView {
             </View>
             <TouchableHighlight onPress={() => {
                 this._onDetailLink(data)
-            }} >
-            <View style={[styles.contentView, {flex: 2}]}>
-                <AIcon name={"angle-right"}
-                       style={{fontSize: 25, alignSelf: "center", color: G_Theme.primary}}/>
-            </View>
+            }}>
+                <View style={[styles.contentView, {flex: 2}]}>
+                    <AIcon name={"angle-right"}
+                           style={{fontSize: 25, alignSelf: "center", color: G_Theme.primary}}/>
+                </View>
             </TouchableHighlight>
 
         </View>)
@@ -94,7 +99,19 @@ export default class LinkListView extends BaseView {
     componentDidMount() {
         G_RunAfterInteractions(() => {
             ActDispatch.FetchAct.fetchVoWithResult(HTTP_SERVER.AgentUserLinkList, (data) => {
-                this.setState({dataList: data.data.datas.data})
+                let result = data.data.datas.data;
+
+                for (let index in result) {
+                    const nowDate = Date.parse(new Date()),
+                        expirtDate = G_DateUtil.datetime2unix(result[index].expired_at);
+                    if (nowDate > expirtDate) {
+                        result[index].status = 2; //过期
+                    }
+                    result[index].price_group = JSON.parse(result[index].prize_group_sets);
+                }
+
+
+                this.setState({dataList: result})
                 this.setState({aStatus: data.data.sources.aStatus})
             })
         })
@@ -104,12 +121,11 @@ export default class LinkListView extends BaseView {
         Clipboard.setString(data);
         G_AlertUtil.show("", "复制成功！")
     }
-    _onDeleteLink=(data)=>{
-        G_RunAfterInteractions(()=>{
-            HTTP_SERVER.AgentUserDelLink.url=HTTP_SERVER.AgentUserDelLink.formatUrl.replace("#id",data.id);
-            ActDispatch.FetchAct.fetchVoWithResult(HTTP_SERVER.AgentUserDelLink,(result)=>{
-                if(result.isSuccess)
-                {
+    _onDeleteLink = (data) => {
+        G_RunAfterInteractions(() => {
+            HTTP_SERVER.AgentUserDelLink.url = HTTP_SERVER.AgentUserDelLink.formatUrl.replace("#id", data.id);
+            ActDispatch.FetchAct.fetchVoWithResult(HTTP_SERVER.AgentUserDelLink, (result) => {
+                if (result.isSuccess) {
                     TLog("G_RunAfterInteractions");
                     let list = this.state.dataList.concat();
                     for (let index in list) {
@@ -126,7 +142,7 @@ export default class LinkListView extends BaseView {
     }
 
     _onDetailLink = (data) => {
-        G_NavUtil.pushToView(G_NavViews.LinkDetailView({content: data,aStatus:this.state.aStatus}))
+        G_NavUtil.pushToView(G_NavViews.LinkDetailView({content: data, aStatus: this.state.aStatus}))
     }
 }
 
